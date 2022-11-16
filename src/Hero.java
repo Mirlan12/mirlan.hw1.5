28578c3d9b1dc3cfab8cedbf79bf67c7230dd2a7
Mirlan12 <mirlan12991@gmail.com>
2022-11-16 16:21:55 +0600

    r

--- src/Hero.java
+++ src/Hero.java
@@ -2,7 +2,7 @@ public class Hero {
     private
     public static int[] heroHealth = {250, 300};
     public static int[] heroDamage = {150, 250};
-
+ggg
 
     public static String[] heroAttack = {"Magic", "Killer"};
 
