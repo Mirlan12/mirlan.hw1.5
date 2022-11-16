d1393ac118a3f586ecd32162bdaa8452d062370d
Mirlan12 <mirlan12991@gmail.com>
2022-11-16 15:56:35 +0600

    Initial commit

---
+++ src/Hero.java
@@ -0,0 +1,8 @@
+public class Hero {
+    private
+    public static int[] heroHealth = {250, 300};
+    public static int[] heroDamage = {150, 250};
+
+    public static String[] heroAttack = {"Magic", "Killer"};
+
+}
